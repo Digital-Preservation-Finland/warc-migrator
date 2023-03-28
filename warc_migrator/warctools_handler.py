27e4f9e4e9e881fc1ebfae6dd13a123687d50487
Vili Oja <vili.oja@csc.fi>
2023-03-28 16:02:18 +0300

    KDKPAS-2918 Remove unneeded workaround for empty field in HTTP header
    Our own patched version of warctools fixes this problem, so the
    workaround in the migrator itself is unnecessary.

--- warc_migrator/warctools_handler.py
+++ warc_migrator/warctools_handler.py
@@ -30,19 +30,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import io
-import datetime
-
 from hanzo.arc2warc import ArcTransformer
 from hanzo.warctools.mixed import MixedRecord
-from hanzo.warctools.warc import WarcRecord, warc_datetime_str
-from hanzo.warctools.arc import ArcRecord
-from hanzo.httptools.messaging import RequestMessage, ResponseMessage
+from hanzo.warctools.warc import WarcRecord
 
 
 def is_arc(source_path):
     """
-    Resolve with Warctools wheter a file is an ARC file or WARC file
+    Resolve with Warctools whether a file is an ARC file or WARC file
 
     :source_path: Archive file path.
     :returns: True for ARC file, False for WARC
@@ -68,7 +63,7 @@ def convert(infile, out):
     :out: WARC file handler
     """
     count = 0
-    arc = Arc2WarcTransformer()
+    arc = ArcTransformer()
     file_handler = MixedRecord.open_archive(filename=infile, gzip="auto")
     try:
         for record in file_handler:
@@ -82,117 +77,3 @@ def convert(infile, out):
         file_handler.close()
 
     return count
-
-
-def is_http_response(content):
-    """
-    Check if the content is a HTTP response.
-
-    The same function is in Warctools, but it raises a ValueError if the
-    Content-Length field is empty in HTTP header. Here we first read the HTTTP
-    haader, and if empty Content-Length is found, it will be ignored.
-
-    :content: Record cotent
-    :returns: True if the content is HTTP response, False otherwise
-    """
-    empty_length = False
-    content_buffer = io.BytesIO(content)
-    for line in content_buffer:
-        if line.strip().lower() == b'content-length:':
-            empty_length = True
-            break
-
-        # This is the way Warctools did it, not going to alter functionality
-        # pylint: disable=len-as-condition
-        if len(line.strip()) == 0:
-            break
-
-    if empty_length:
-        message = ResponseMessage(RequestMessage(),
-                                  ignore_headers=[b"Content-Length"])
-    else:
-        message = ResponseMessage(RequestMessage())
-
-    remainder = message.feed(content)
-    message.close()
-    return message.complete() and not remainder
-
-
-# pylint: disable=no-member, too-many-branches, trailing-comma-tuple
-# WarcRecord has constants in a decorator, which is not supoorted in pylint.
-class Arc2WarcTransformer(ArcTransformer):
-    """
-    Override Warctools class ArcTransformer.
-    """
-
-    def convert_record(self, record):
-        """
-        Convert ARC record to WARC.
-
-        This method overrides ArcTransformer.convert_record() found in
-        Warctools. This method is no different from the method it
-        overrides (except some style fixes), but here it uses the
-        is_http_response() function defined in this module.
-
-        :record: ARC record
-        :returns: Converted WARC record(s)
-        """
-        warc_id = self.make_warc_uuid(record.url+record.date)
-        headers = [
-            (WarcRecord.ID, warc_id),
-            (WarcRecord.URL, record.url),
-            (WarcRecord.WARCINFO_ID, self.warcinfo_id),
-        ]
-
-        if record.date:
-            try:
-                date = datetime.datetime.strptime(
-                    record.date.decode('ascii'), '%Y%m%d%H%M%S')
-            except ValueError:
-                date = datetime.datetime.strptime(
-                    record.date.decode('ascii'), '%Y%m%d')
-        else:
-            date = datetime.datetime.now()
-
-        host = record.get_header(ArcRecord.IP)
-        if host:
-            host = host.strip()
-            if host != b"0.0.0.0":
-                headers.append((WarcRecord.IP_ADDRESS, host))
-
-        headers.append((WarcRecord.DATE, warc_datetime_str(date)))
-
-        content_type, content = record.content
-
-        if not content_type.strip():
-            content_type = b'application/octet-stream'
-
-        url = record.url.lower()
-
-        if any(url.startswith(p) for p in self.resources):
-            record_type = WarcRecord.RESOURCE
-        elif any(url.startswith(p) for p in self.responses):
-            record_type = WarcRecord.RESPONSE
-        elif url.startswith(b'http'):
-            if is_http_response(content):
-                content_type = b"application/http;msgtype=response"
-                record_type = WarcRecord.RESPONSE
-            else:
-                record_type = WarcRecord.RESOURCE
-        elif url.startswith(b'dns'):
-            if content_type.startswith(b'text/dns') and \
-                    str(content.decode('ascii', 'ignore')) == content:
-                record_type = WarcRecord.RESOURCE
-            else:
-                record_type = WarcRecord.RESPONSE
-        else:
-            # unknown protocol
-            record_type = WarcRecord.RESPONSE
-
-        headers.append((WarcRecord.TYPE, record_type))
-
-        warcrecord = WarcRecord(
-            headers=headers, content=(content_type, content),
-            version=self.version)
-
-        return warcrecord,
